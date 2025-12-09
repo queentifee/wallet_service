388361e1594005408a790d176964a040e4cf8d6c
Queen <samuellqueenie@gmail.com>
2025-12-10 00:44:39 +0100

    chore(deployment) prepare for render deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,14 +16,17 @@ import { Transaction } from './entities/transaction.entity';
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
-      port: Number(process.env.DATABASE_PORT) || 5432,
-      username: process.env.DB_USER || 'postgres',
-      password: process.env.DB_PASSWORD || 'postgres',
-      database: process.env.DB_NAME || 'wallet_system',
+      // host: process.env.DB_HOST || 'localhost',
+      // port: Number(process.env.DATABASE_PORT) || 5432,
+      // username: process.env.DB_USER || 'postgres',
+      // password: process.env.DB_PASSWORD || 'postgres',
+      // database: process.env.DB_NAME || 'wallet_system',
+        url: process.env.DATABASE_URL, // Single connection string
       entities: [User, ApiKey, Wallet, Transaction],
   synchronize: false, 
-  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
+ ssl: {
+    rejectUnauthorized: false, // Always use SSL on Render
+  },
     }),
     AuthModule,
     KeysModule,
